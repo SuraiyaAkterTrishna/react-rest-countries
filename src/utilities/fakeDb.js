a65f990d55fa8477d1eaa74e755a27c136170d66
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-19 19:41:38 +0600

    add shopping cart to local storage done

--- src/utilities/fakeDb.js
+++ src/utilities/fakeDb.js
@@ -1,11 +1,56 @@
 const addToDb = (id) => {
-    if(localStorage.getItem(id)){
-        const quantity = localStorage.getItem(id);
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+    let shoppingCart;
+    const storedCart = localStorage.getItem('shopping-cart');
+
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart);
     }
     else{
-        localStorage.setItem(id, 1);
+        shoppingCart = {};
     }
+    
+    const quantity = shoppingCart[id];
+    if(quantity){
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }
+    else{
+        shoppingCart[id] = 1;
+    }
+
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 };
 export {addToDb};
+
+
+
+
+
+
+
+
+
+
+
+
+/* const addToDb = (id) => {
+    let shoppingCart;
+
+    const storedCart = localStorage.getItem('shopping-cart');
+    if(storedCart){
+        shoppingCart = JSON.parse(storedCart); 
+    }
+    else{
+        shoppingCart = {};
+    }
+    const quantity = shoppingCart[id];
+    if(quantity){
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }
+    else{
+        shoppingCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+};
+export {addToDb}; */
