f00c9e55d067a544604a4d3f90874ca3aae0d84f
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-19 17:22:10 +0600

    load fake data from data.json file

--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -1,51 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Cosmetic from "../Cosmetic/Cosmetic";
 
 const Cosmetics = () => {
-  /* const cosmetics = [
-        {id: 1, name: 'Alta', price: 100},
-        {id: 2, name: 'Churi', price: 200},
-        {id: 3, name: 'Nailpolis', price: 300},
-        {id: 4, name: 'Lipstick', price: 400},
-        {id: 5, name: 'Ring', price: 500},
-    ]; */
-  const cosmetics = [
-    {
-      _id: "6490392ea9406573b3409e4e",
-      price: 27,
-      name: "Hodges Alexander",
-    },
-    {
-      _id: "6490392ecd78ae8d7bbb08bd",
-      price: 26,
-      name: "Geraldine Bauer",
-    },
-    {
-      _id: "6490392e7bf1e9f380b52a2b",
-      price: 146,
-      name: "Shannon Newman",
-    },
-    {
-      _id: "6490392e2a65f942db51f978",
-      price: 188,
-      name: "Lacey Cox",
-    },
-    {
-      _id: "6490392e7f33806b08a82f3b",
-      price: 263,
-      name: "Graves Landry",
-    },
-    {
-      _id: "6490392ed05604d4838dd617",
-      price: 32,
-      name: "Bartlett Hayes",
-    },
-    {
-      _id: "6490392e3ebe599ccc993d32",
-      price: 222,
-      name: "Caitlin Wong",
-    },
-  ];
+ const [cosmetics, setCosmetics] = useState([]);
+ useEffect( () => {
+    fetch('data.json')
+    .then(res => res.json())
+    .then(data => setCosmetics(data))
+ } , []);
   return (
     <div>
       {cosmetics.map((cosmetic) => (
