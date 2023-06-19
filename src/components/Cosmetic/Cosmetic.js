d7334ac76d9acac34e3578bd82ed2a23becd2b29
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-06-19 17:49:03 +0600

    add event handler with parameter

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import './Cosmetic.css';
 
 const Cosmetic = (props) => {
     const {name, price, _id} = props.cosmetic;
+    const addToCart = (id) => {
+        console.log("item added", id);
+    }
+    // const addToCartWithParams = () => addToCart(_id);
     return (
-        <div>
+        <div className='cosmetic'>
             <h1>Name: {name}</h1>
             <p>Price: {price}</p>
             <small>Id: {_id}</small>
-
+            <br />
+            {/* <button onClick={addToCartWithParams}>Add to card</button> */}
+            <button onClick={() => addToCart(_id)}>Add to card</button>
         </div>
     );
 };
--- src/components/Cosmetics/Cosmetics.js
+++ src/components/Cosmetics/Cosmetics.js
@@ -2,16 +2,19 @@ import React, { useEffect, useState } from "react";
 import Cosmetic from "../Cosmetic/Cosmetic";
 
 const Cosmetics = () => {
- const [cosmetics, setCosmetics] = useState([]);
- useEffect( () => {
-    fetch('data.json')
-    .then(res => res.json())
-    .then(data => setCosmetics(data))
- } , []);
+  const [cosmetics, setCosmetics] = useState([]);
+  useEffect(() => {
+    fetch("data.json")
+      .then((res) => res.json())
+      .then((data) => setCosmetics(data));
+  }, []);
   return (
     <div>
       {cosmetics.map((cosmetic) => (
-        <Cosmetic cosmetic={cosmetic}></Cosmetic>
+        <Cosmetic 
+        key={cosmetic._id} 
+        cosmetic={cosmetic}
+        ></Cosmetic>
       ))}
     </div>
   );
